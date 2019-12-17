d9edab3d4dbcccd658ba20fe4ea94c890c31a782
Theodor Isacsson <theodori@kth.se>
2019-12-16 23:25:10 -0500

    Add ideas to test

--- qecco/main.py
+++ qecco/main.py
@@ -1,15 +1,15 @@
 # TODO: Add save function rhos/bosonic codes in JSON
 
-# TODO: Get the "autocodec" to work
-# TODO: Make decoder able to test over different eta
-
 # TODO: Create a print-out of the system (with inputs, outputs, and ancillae); curses?!
 # TODO: Expand main to ask for all important parameter inputs
-# TODO: Reorganize the functions
 # TODO: Add nice-exit when pressing ctrl-c
 # TODO: Follow same convention for all assertions (e.g. in system.get_output_rhos)
 
 # TODO: Add parallelization
+
+# IDEA: Check what happens if input non-loss states or single-photon-loss states
+# E.g input (|40> + |04>) / sqrt(2) and |30> should give same (see photo on cellphone 901210)
+# IDEA: Try dropout (remove layers during training)
 
 import sys
 from .optimize import optimize, opt_type_inputs
